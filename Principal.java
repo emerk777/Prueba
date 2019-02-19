6a227a4fe0f6a58505102f8adfab33275a48c8c9
Emersson De Leon <emerk777@yahoo.com>
2019-02-19 17:18:32 -0600

    Agregando Fecha Actual

--- Principal.java
+++ Principal.java
@@ -1,8 +1,15 @@
+import java.util.*;
+import java.text.*;
+
 public class Principal{
 
 public static void main(String[] args) {
 
   System.out.println("Hola Mundo...  ... ahora el cambio luego del commit inicial");
+
+  DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+  Date date = new Date();
+  System.out.println(dateFormat.format(date));
 
 }
 
